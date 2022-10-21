6872566ef61abf4f415deba5be5e5d92b67b119c
Muhammed380 <muhammedgoksu33@icloud.com>
2022-10-21 11:19:59 +0300

    Added splashfragment

---
+++ app/src/main/java/com/mami/horoscopeapp/ui/splash/SplashFragment.kt
@@ -0,0 +1,39 @@
+package com.mami.horoscopeapp.ui.splash
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.mami.horoscopeapp.R
+import com.mami.horoscopeapp.databinding.FragmentSplashBinding
+import com.mami.horoscopeapp.ui.base.BaseFragment
+
+
+class SplashFragment : BaseFragment<FragmentSplashBinding>() {
+    private lateinit var countDownTimer: CountDownTimer
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        splashScreen()
+    }
+
+    private fun splashScreen() {
+
+        countDownTimer = object : CountDownTimer(3000, 1000) {
+            override fun onTick(p0: Long) {}
+            override fun onFinish() {
+                findNavController().navigate(R.id.homeFragment)
+            }
+        }.start()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        if (this::countDownTimer.isInitialized)
+            countDownTimer.cancel()
+    }
+
+    override fun getFragmentView(): Int = R.layout.fragment_splash
+}
